3b3a8f78e53714801ef2633ed8ce67a89f524e2f
Vatsal Parmar <vparmar@sfu.ca>
2022-06-02 01:45:04 -0700

    New stuff

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 import config from '../config/index.json';
 
 const About = () => {
-  const { company, about } = config;
+  const { company } = config;
   const { logo, name: companyName } = company;
-  const { socialMedia, sections } = about;
 
   return (
     <div
@@ -16,7 +15,7 @@ const About = () => {
         <div>
           <img src={logo} alt={companyName} className="w-16 h-16" />
         </div>
-        <div className="flex flex-wrap sm:gap-10 gap-8 items-center justify-center mt-4 h-12">
+        {/* <div className="flex flex-wrap sm:gap-10 gap-8 items-center justify-center mt-4 h-12">
           {sections.map((section, index) => (
             <a
               key={`${section.name}-${index}`}
@@ -26,8 +25,8 @@ const About = () => {
               {section.name}
             </a>
           ))}
-        </div>
-        <div className="flex items-center gap-x-8 mt-6 h-8">
+        </div> */}
+        {/* <div className="flex items-center gap-x-8 mt-6 h-8">
           <a
             aria-label="github"
             href={socialMedia.github}
@@ -76,14 +75,14 @@ const About = () => {
               <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.066 9.645c.183 4.04-2.83 8.544-8.164 8.544-1.622 0-3.131-.476-4.402-1.291 1.524.18 3.045-.244 4.252-1.189-1.256-.023-2.317-.854-2.684-1.995.451.086.895.061 1.298-.049-1.381-.278-2.335-1.522-2.304-2.853.388.215.83.344 1.301.359-1.279-.855-1.641-2.544-.889-3.835 1.416 1.738 3.533 2.881 5.92 3.001-.419-1.796.944-3.527 2.799-3.527.825 0 1.572.349 2.096.907.654-.128 1.27-.368 1.824-.697-.215.671-.67 1.233-1.263 1.589.581-.07 1.135-.224 1.649-.453-.384.578-.87 1.084-1.433 1.489z" />
             </svg>
           </a>
-        </div>
+        </div> */}
         <div className="flex items-center mt-6">
           <p className="mt-6 text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">
-            &copy; {new Date().getFullYear()} designed by{' '}
-            <a href="https://github.com/issaafalkattan" rel="nofollow">
-              Issaaf Kattan
-            </a>
+            102 - 2909 Bainbridge Ave Burnaby BC V5B 2S9
           </p>
+        </div>
+        <div className="flex items-center mt-6">
+          <p>Any Questions? Call us: 604-551-0896</p>
         </div>
       </div>
     </div>
